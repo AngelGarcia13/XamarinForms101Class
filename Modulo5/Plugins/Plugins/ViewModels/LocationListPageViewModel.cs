b07758ce5820623c6b639e42cbc9461c26d36300
Angel Garcia <angel.rene.garcia13@gmail.com>
2018-08-05 22:22:26 -0400

    Added Module 5

---
+++ Modulo5/Plugins/Plugins/ViewModels/LocationListPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Plugins.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Realms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Plugins.ViewModels
+{
+	public class LocationListPageViewModel : ViewModelBase
+	{
+        private readonly Realm _realm;
+        public LocationListPageViewModel(INavigationService navigationService, Realm realm)
+            : base(navigationService)
+        {
+            Title = "Saved List";
+            _realm = realm;
+            Locations = new ObservableCollection<UserLocation>();
+            LoadLocationsFromLocalDatabase();
+        }
+
+        private void LoadLocationsFromLocalDatabase()
+        {
+            IsRunning = true;
+            var allLocations = _realm.All<UserLocation>();
+            foreach (var item in allLocations)
+            {
+                Locations.Add(item);
+            }
+            IsRunning = false;
+
+        }
+
+        private ObservableCollection<UserLocation> locations;
+        public ObservableCollection<UserLocation> Locations
+        {
+            get { return locations; }
+            set { SetProperty(ref locations, value); }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Módulo 2 https://drive.google.com/open?id=1TeFYh3ZLC64CT4ESwSRz6grhEGg2mZoOJxvw
 Módulo 3 https://drive.google.com/open?id=1HisoyLS5lkrzBKBL2kJcQH19YBkk1Lyi1MTjniZ7L4o
 
 Módulo 4 https://drive.google.com/open?id=14UFE2Gz-OSL94vRO2wpID41db_Kb7hcEFuR-DMj-xjw
+
+Módulo 5 https://drive.google.com/open?id=1MfeLKEhQPWfMDwwGlevJuv0NgZ5-cbJoeMoPqUyxfY4
